d7a2011cd8f14c71a161bcc6291d8d5e4b86c4cb
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-03-19 13:47:05 +0800

    initial

---
+++ .vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+import { sidebar } from './sidebar'
+
+export default defineConfig({
+    // These are app level configs.
+    title: 'kbyakuya',
+    description: 'kbyakuya',
+    base: "/",
+    lastUpdated: true,
+    cleanUrls: 'without-subfolders',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/favicon.ico',
+        }],
+        ["link", {
+            rel: "stylesheet",
+            href: "//unpkg.com/heti/umd/heti.min.css",
+        },],
+        ['meta', { name: 'theme-color', content: '#3c8772' }],
+        // ['meta', { name: 'theme-color', content: '#ff64b9' }],
+    ],
+    themeConfig: {
+        logo: '/logo/logo.jpg',
+        // sidebar: sidebar,
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/kbyakuya/kbyakuya.github.io' },
+        ],
+        editLink: {
+            pattern: 'https://github.com/kbyakuya/kbyakuya.github.io/tree/master/article/:path',
+            text: 'Suggest changes to this page',
+        },
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2023-present kbyakuya'
+        },
+
+    }
+})
---
+++ index.md
@@ -0,0 +1,23 @@
+---
+layout: home
+title: kbyakuya
+titleTemplate: record something
+hero:
+  name: kbyakuya
+  text: record something
+  tagline: 见证者，为见证而来 | 铭记者，因铭记而生
+  image:
+    src: /img/home.jpg
+    alt: none
+  actions:
+    - theme: brand
+      text: Leadership Principles
+      link: https://augusmeow.github.io/notes/career/amazon/leadership-principles/
+    - theme: alt
+      text: todo
+      link: /       
+# Could add features description here
+features:
+  - title: Leadership Principles
+    details: We use our Leadership Principles every day, whether we’re discussing ideas for new projects or deciding on the best way to solve a problem. It’s just one of the things that makes Amazon peculiar.
+---
